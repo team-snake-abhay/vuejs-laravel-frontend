1a0b35a5d2352486ec79fa121a11350b249c2210
team-snake-abhay <137865227+team-snake-abhay@users.noreply.github.com>
2023-09-28 17:40:25 +0530

    Update vuejs-env.sh

--- frontend/vuejs-env.sh
+++ frontend/vuejs-env.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 read -p "Enter URL: " laravelurl
-
-# Remove the trailing slash if it exists
 laravelurl=${laravelurl%/}
 
 # Append "/v1/api" to the URL
 url_with_api="$laravelurl/api/v1"
 
+# Full path to the .env file
+env_file="$HOME/public_html/vuejs-laravel-frontend/frontend/.env"
+
 # Update the .env file
-# sed -i "s/^API_URL_DEV=.*/API_URL_DEV=$url_with_api/" .env
-# sed -i "s/^API_URL=.*/API_URL=$url_with_api/" .env
+sed -i "s/^API_URL_DEV=.*/API_URL_DEV=$url_with_api/" "$env_file"
+sed -i "s/^API_URL=.*/API_URL=$url_with_api/" "$env_file"
